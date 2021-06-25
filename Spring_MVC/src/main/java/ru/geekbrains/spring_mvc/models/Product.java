36aa1e103e7844664a80a857519c0201bf096352
VadimHome <vadimtsh@gmail.com>
2021-06-25 19:16:05 +0300

    HomeWork_3

--- Spring_MVC/src/main/java/ru/geekbrains/spring_mvc/models/Product.java
+++ Spring_MVC/src/main/java/ru/geekbrains/spring_mvc/models/Product.java
@@ -2,11 +2,13 @@ package ru.geekbrains.spring_mvc.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 
 public class Product {
     private int id;
--- Spring_MVC/src/main/java/ru/geekbrains/spring_mvc/repositories/ProductRepository.java
+++ Spring_MVC/src/main/java/ru/geekbrains/spring_mvc/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.spring_mvc.repositories;
 
+import org.springframework.stereotype.Component;
 import ru.geekbrains.spring_mvc.models.Product;
 
 import javax.annotation.PostConstruct;
@@ -8,16 +9,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-
+@Component
 public class ProductRepository {
     private List<Product> werehouse;
 
     @PostConstruct
-    public void init(){
-        Random r=new Random();
-        this.werehouse=new ArrayList();
+    public void init() {
+        Random r = new Random();
+        this.werehouse = new ArrayList();
         for (int i = 0; i < 10; i++) {
-            this.werehouse.add(new Product(i,"Product_"+i,100+(10* r.nextDouble())));
+            this.werehouse.add(new Product(i, "Product_" + i, 100 + (10 * r.nextDouble())));
         }
 
     }
@@ -28,10 +29,19 @@ public class ProductRepository {
 
 
     public Product getInd(int ind) throws IndexOutOfBoundsException {
-        if (ind<0 || ind>=this.werehouse.size()){
+        if (ind < 0 || ind >= this.werehouse.size()) {
             throw new IndexOutOfBoundsException();
         }
         return this.werehouse.get(ind);
+    }
+
+    public Product findByID(int id) {
+        for (Product p : this.werehouse) {
+            if (p.getId()==id){
+                return p;
+            }
+        }
+        throw new IllegalArgumentException("Product not fond");
     }
 
 
@@ -44,12 +54,12 @@ public class ProductRepository {
         return this.werehouse.remove(prod);
     }
 
-    public void setInd(int ind, Product prod) throws IndexOutOfBoundsException{
-        if (ind<0 || ind>=this.werehouse.size()){
+    public void setInd(int ind, Product prod) throws IndexOutOfBoundsException {
+        if (ind < 0 || ind >= this.werehouse.size()) {
             throw new IndexOutOfBoundsException();
         }
         Product tmp;
-        tmp=this.werehouse.get(ind);
+        tmp = this.werehouse.get(ind);
         tmp.setCost(prod.getCost());
         tmp.setTitle(prod.getTitle());
     }
